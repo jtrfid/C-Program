b3a6de48ebedaf12365e356de7f5ac53c84fea3a
jtrfid <jtrfid@qq.com>
2020-10-13 22:48:49 +0800

    update

--- 2020/latex/课堂讲解/Lecture-4.cpp
+++ 2020/latex/课堂讲解/Lecture-4.cpp
@@ -1,44 +1,87 @@
 #include<stdio.h>
-#include<math.h> 
+#include<math.h>
+ 
 int main1()
 {
-	int a=0x0123,b,sum;
-	float c;
-	double d;
-	char e;
-	unsigned int f=0xFFFFFFFB;
-	unsigned char g=0xFB,g1=0xFF;
-	printf("%X,%d,%u,%d\n",g,g,g,15*16+11);
-	printf("%X,%d,%u,%d\n",g1,g1,g1,15*16+15);
-	a=-5; 
-	b=456;
-	sum=a+b;
-	//printf ("%X,%d,%d\n",a,a,sum);
-	scanf("%f%c%lf",&c,&e,&d);
-	// printf("%f,%lf,%c\n",c,d,e);
+	double x;
+	char c2=-5,c3=0xFB;
+	int a=-5,b,c,d;
+	float y;
+	unsigned char c1=-5;
+	unsigned int a1=0XFFFFFFFB, a2=0xFB, a3=-5;
+	
+	printf("%X,%X,%X\n",a1,a2,a3); // FFFFFFFB,FB,FFFFFFFB
+	
+	printf("%c,%d,%X,%c,%c\n",'?','?','?',0X3F,0XFB);
+	
+	printf("%X,%d,%c\n",c3,c3,c3); // FFFFFFFB,-5,?
+	
+	printf("%d,%d\n",15*16+11,a);	
+	
+	printf("%X,%d,%X,%d,%d\n",c1,c1,c2,c2); // FB,251,FFFFFFFB,-5
+	
+	//scanf("%d%c%d%d%lf%f",&a,&z,&b,&c,&x,&y);
+	//scanf("%d",&b);
+	//scanf("%d",&c); 
+	//printf("%d,%d,%d,%c,%.3lf,%.2f\n",a,b,c,z,x,y);
 	return 0;
-
 }
 
-// +,-,*,/, a%b求a/b的余数 
-// a++,++a;
-// int abs(int x); 函数原型 
+// 算术表达式，+，-，*，/ 
+int main2()
+{
+	double x=10.0;
+	char c2=-5,c3='s';
+	int a=-5,b,c,d;
+	float y=10.5,y1;
+	unsigned char c1=-5;
+	unsigned int a1=0XFFFFFFFB, a2=0xFB, a3=-5;
+	
+	c=a*(y+10); // 结果是整数,不四舍五入 
+	y1=a*y+10;
+	y1=x*c3; // double -> float
+	
+	printf("%d,%f,%d\n",c,y1,c3); // -42,1150.000000,115
+	
+	c=a/10; // 整数/整数--->结果是整数
+	
+	y1=a/10;  // 0.0
+	
+	y1=a/10.0; // 0.5 
+	
+    y1=(float)a/10; // 在表达式内，转换a为float 
+	
+	printf("%d,%d\n",c,a);
+	
+	return 0;
+}
+
+// i++,++i; i--,--i
+int main3()
+{
+	int i=0,a;
+	
+	a=i++; // i: 先使用，后加1 
+	
+	printf("%d,%d\n",a,i); // 0,1
+	
+	a=++i; // i: 先加1，后使用 
+	
+	printf("%d,%d\n",a,i); // 2,2
+	
+	i++; // i=i+1 
+	
+	return 0;
+}
+
+// 数学函数 
+// int abs(int x); double fabs(double x)
 int main()
 {
-	int a=-11,b=21,sum;
-	int i=5;
-	float c=10.5;
-	double d;
-	char e='A';
-	printf("%d\n",abs(a));
-	d=a*(c+10)+e;
-	b=a*c+10+e;
-	d=a/b; // 整数/整数 ---- 结果是整数 
-	printf("d=%lf,%d,%d,%d,%d\n",d,b,e,a%10,1/2);
-	b=a++;  // a++先使用a, 本身再加1 
-	printf("%d,%d\n",a,b); // 12,11
-	b=++i; // 先加1, 再使用 
-	printf("%d,%d\n",i,b); // 6,6
+	int a=-10;
+	float x=10.0;
+	
+	printf("%d,%f\n",abs(a),fabs(x));
+	
 	return 0;
-
 }
